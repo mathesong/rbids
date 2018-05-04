2d68525b585b20c89f36ebcaa0453b0e10c2a09a
Pontus Plavén Sigray <pontus.plaven-sigray@ki.se>
2018-05-04 15:37:39 +0200

    Fun to visulize  folder structure of BIDS project as a dendrogram

---
+++ R/visualize_layout.R
@@ -0,0 +1,28 @@
+#' Title
+#'
+#' @param studypath path to BIDS project
+#' @param write_to_csv_name csv file name (optional)
+#' @return a data.tree S6 object outlining the project layout
+#' @export
+#'
+#' @examples
+#' visualize_layout(studypath = getwd(), write_to_csv_name =paste0(getwd(),'/BIDslayout.csv')  )
+visualize_layout <- function(studypath, write_to_csv_name=NULL) {
+  files <- list.files(path,
+    pattern = ".json",
+    recursive = T
+  )
+  df <- data.frame(
+    filename = sapply(
+      files,
+      function(fl) paste0("layout", "/", fl)
+    ),
+    file.info(paste(path, files, sep = "/")),
+    stringsAsFactors = FALSE
+  )
+  fileStructure <- data.tree::as.Node(df, pathName = "filename")
+  if (!is.null(write_to_csv_name)) {
+    write.table(fileStructure, file = write_to_csv_name, row.names = F, col.names = F)
+  }
+  fileStructure
+}
