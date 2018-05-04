43bc353a3eabb7973c88b04fd314c47febb24cf6
Pontus Plavén Sigray <pontus.plaven-sigray@ki.se>
2018-05-04 16:02:31 +0200

    Rm write to csv option, made fun windows compatible

--- R/vis_layout.R
+++ R/vis_layout.R
@@ -1,28 +1,26 @@
 #' Title
 #'
 #' @param studypath path to BIDS project
-#' @param write_to_csv_name csv file name (optional)
 #' @return a data.tree S6 object outlining the project layout
 #' @export
 #'
 #' @examples
-#' vis_layout(studypath = getwd(), write_to_csv_name =paste0(getwd(),'/BIDslayout.csv')  )
-vis_layout <- function(studypath, write_to_csv_name=NULL) {
-  files <- list.files(path,
-    pattern = ".json",
+#' vis_layout(studypath = getwd())
+vis_layout <- function(studypath) {
+  files <- fs::dir_info(studypath,
     recursive = T
-  )
+  ) %>%
+    dplyr::mutate(relpath = fs::path_rel(path, studypath))
+
   df <- data.frame(
     filename = sapply(
-      files,
+      files$relpath,
       function(fl) paste0("layout", "/", fl)
     ),
-    file.info(paste(path, files, sep = "/")),
+    file.info(paste(studypath, files$relpath, sep = "/")),
     stringsAsFactors = FALSE
   )
+
   fileStructure <- data.tree::as.Node(df, pathName = "filename")
-  if (!is.null(write_to_csv_name)) {
-    write.table(fileStructure, file = write_to_csv_name, row.names = F, col.names = F)
-  }
   fileStructure
 }
